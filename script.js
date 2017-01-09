deac62455171ed1433df8dc1e29cdab96153bddd
Jeff Saquipulla <jeffs2167@queensvoc.org>
2017-01-09 22:24:22 +0000

    smiley face, 4  Corners

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Write your function calls below this line
+setColor(9,0,  "purple");
+setColor(0, 0, "blue");
+setColor(0, 9, "green");
+setColor(9, 9, "yellow");
+
+setColor(1,3, "red")
+setColor(1,6, "red")
+setColor(3,2, "red")
+setColor(4,4, "red")
+setColor(4,5, "red")
+setColor(3,7, "red")
+
